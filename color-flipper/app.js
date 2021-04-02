9af3bffe7e179fe2c93ed4ec890e0e26ff759c0b
ishika <imusun24@gmail.com>
2021-04-02 16:30:46 +0530

    update

---
+++ color-flipper/app.js
@@ -0,0 +1,7 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  console.log(document.body);
+});
