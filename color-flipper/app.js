b63a8b80a5a271f9145fd1217e3533c017201f50
ishika <imusun24@gmail.com>
2021-04-03 21:30:48 +0530

    yo

--- color-flipper/app.js
+++ color-flipper/app.js
@@ -1,7 +1,9 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
+const btn = document.querySelector("#btn");
 
-btn.addEventListener("click", function () {
-  console.log(document.body);
+btn.addEventListener("click", () => {
+  document.body.style.backgroundColor = `rgb(${Math.floor(
+    Math.random() * 256
+  )},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
+  
+
 });
